d2fc0585371bc7b2e6110c19d7677d49c5578ab9
MaxxyDaWolf <whalemo71@gmail.com>
2025-06-20 07:23:54 -0400

    Update pladmin.lua

--- pladmin.lua
+++ pladmin.lua
@@ -21,4 +21,4 @@ PLadmin_Settings = {
   RankedOutput = true; --Chat the output commands of ranked players
   WhisperToRanked = false; --Use whisper for outputing commands for ranked players
 };wait()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/wrtrteyteey23ee/PrizzLife/refs/heads/main/Source/release_v0.8.1.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/devguy100/PrizzLife/main/pladmin.lua'))()
