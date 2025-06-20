efbf0ede68c770e5be713d046ce457b6c84e11ab
MaxxyDaWolf <whalemo71@gmail.com>
2025-06-19 21:26:03 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IsiahEngineer/GOL/refs/heads/main/OWTBACKWARDS"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGooferFox/adsdasd/refs/heads/main/prision%20life.lua?token=GHSAT0AAAAAADAEZMYPZHIBR5HRO7PVR44E2CUWTZA", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheGooferFox/asdssda/refs/heads/main/script.lua", true))()
